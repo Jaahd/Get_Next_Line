6d56c6da07f6757c56f7648f96b8f63ee154775d
avacher <avacher@student.42.fr>
2016-01-06 11:01:02 +0100

    norme ok

--- get_next_line.h
+++ get_next_line.h
@@ -6,14 +6,14 @@
 /*   By: avacher <avacher@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/10 12:16:40 by avacher           #+#    #+#             */
-/*   Updated: 2015/12/28 16:39:44 by avacher          ###   ########.fr       */
+/*   Updated: 2016/01/06 11:00:45 by avacher          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# define BUFF_SIZE 1024 
+# define BUFF_SIZE 1024
 
 typedef struct		s_line
 {
